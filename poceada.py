04d7c59e20b65042941fa26c075cfffcf9fc50ed
Dure Matias Miguel <72953094+MatiaSalvatore@users.noreply.github.com>
2026-05-06 13:39:35 -0300

    Comentarios

--- poceada.py
+++ poceada.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup as Bs
 import pandas as pd
 
+#FUNCION PARA EXTRAER SORTEOS
+#FUNCTION TO EXTRACT DRAWS
+
 def extraer_sorteos(id_sorteo):
     #Definición de la url y headers para la extracción, la idea es extraer ingresando el id de la página, que almacena todos los sorteos.
     #Defining the URL and headers for extraction; the idea is to extract by entering the page ID, which stores all the draws.
@@ -28,14 +31,19 @@ def extraer_sorteos(id_sorteo):
 
 
         #Extracción de números
+        #Number extraction
+        #Primero hago una búsqueda de la ul que contiene los números y chequeo que exista.
         numeros_ul = parser.find('ul', class_='results-list')
         if not numeros_ul:
             return None
+        #Busco cada elemento li dentro de numeros_ul.
         items = numeros_ul.find_all('li', class_='results-list__item')
         numeros = []
         for i in items:
             if 'headers' in i.get('class', []):
                 continue
+            #Buscos los elementos p dentro de cada li
+            #Como son dos p por li, hago una condición para que me devuelva solamente la columna que corresponde a los resultados.
             parrafos = i.find_all('p',class_='results-number')
             if len(parrafos)>=2:
                 numero = parrafos[1].get_text(strip=True)
@@ -45,4 +53,7 @@ def extraer_sorteos(id_sorteo):
         return {"fecha": fecha_sorteo, "numeros": numeros}
     except Exception:
         return None
+#TEST
 print(extraer_sorteos(934))
+
+#
